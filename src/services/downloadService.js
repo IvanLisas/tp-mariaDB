d1ab198ab1780938a1a5a0515f54db846efe42a6
marianobottazzi <bottazzimariano@gmail.com>
2020-12-05 19:15:45 -0300

    adding stats functions to services

--- src/services/downloadService.js
+++ src/services/downloadService.js
@@ -1,18 +1,44 @@
-import axios from 'axios'
-import { REST_SERVER_URL } from './constants'
-import { Download } from '../domain/action'
+import axios from "axios"
+import { REST_SERVER_URL } from "./constants"
+import { Download } from "../domain/action"
 
 class DownloadService {
 
-  async averageDownload(userID) {
-    const json = await axios.get(`${REST_SERVER_URL}/averageDownload/${userID}`)
+  async downloadsOfMonth(userID) {
+    const json = await axios.get(
+      `${REST_SERVER_URL}/downloadsByDate/${userID}/${month}/${year}`
+    )
+    return json.data
+  }
+
+  async downloadsLast12Months(userID) {
+    const json = await axios.get(
+      `${REST_SERVER_URL}/downloadsLast12Months/${userID}`
+    )
+    return json.data
+  }
+
+  async downloadsAverage(userID) {
+    const json = await axios.get(
+      `${REST_SERVER_URL}/downloadsAverage/${userID}`
+    )
+    return json.data
+  }
+
+  async downloadSpeedAvg(userID) {
+    const json = await axios.get(
+      `${REST_SERVER_URL}/downloadSpeedAvg/${userID}`
+    )
     return json.data
   }
 
   async searchDownloadsOf(userID, filtros, orden) {
     console.log(filtros, orden)
-    const json = await axios.put(`${REST_SERVER_URL}/searchDownloadsOf/${userID}`, { filtros: filtros, orden: orden })
-    return json.data.map(download => Download.fromJSON(download))
+    const json = await axios.put(
+      `${REST_SERVER_URL}/searchDownloadsOf/${userID}`,
+      { filtros: filtros, orden: orden }
+    )
+    return json.data.map((download) => Download.fromJSON(download))
   }
 
   // async allDownloads(userID) {
--- src/services/reproductionService.js
+++ src/services/reproductionService.js
@@ -4,10 +4,33 @@ import { REST_SERVER_URL } from './constants'
 import { Reproduction } from '../domain/action'
 
 class ReproductionService {
+
+  async reproductionsOfMonth(userID) {
+    const json = await axios.get(
+      `${REST_SERVER_URL}/reproductionsByDate/${userID}/${month}/${year}`
+    )
+    return json.data
+  }
+
+  async reproductionsLast12Months(userID) {
+    const json = await axios.get(
+      `${REST_SERVER_URL}/reproductionsLast12Months/${userID}`
+    )
+    return json.data
+  }
+
+  async reproductionsAverage(userID) {
+    const json = await axios.get(
+      `${REST_SERVER_URL}/reproductionAverage/${userID}`
+    )
+    return json.data
+  }
+
   async searchReproductionsOf(userID, filtros) {
     const json = await axios.put(`${REST_SERVER_URL}/searchReproductionsOf/${userID}`, filtros)
     return json.data.map(reproducion => Reproduction.fromJSON(reproducion))
   }
+
 }
 
 export const reproductionService = new ReproductionService()
